6a9d415ab2e4f53d1aa5fbebd7d5e55670e58d29
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-07-04 18:38:55 -0700

    update useEffect in searchBook.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -26,8 +26,8 @@ const SearchBooks = () => {
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
-    return () => saveBookIds(savedBookIds);
-  });
+     saveBookIds(savedBookIds);
+  },[savedBookIds]);
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
