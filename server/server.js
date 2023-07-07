24a710ba84e1fb1e6df4afbfb3f79898493bfc69
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-07-06 22:22:56 -0700

    add route handler for the root URL (/)

--- server/server.js
+++ server/server.js
@@ -22,10 +22,18 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+app.use(express.static(buildPath));
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // initializes the ApolloServer and performs any necessary setup operations.
 const startServer = async () => {
