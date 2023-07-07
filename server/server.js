732cdfb0305fca7586f00a670069c1aab88b87ac
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-07-06 22:40:28 -0700

    added engine obj in package.json

--- server/server.js
+++ server/server.js
@@ -22,15 +22,13 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
-const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../client/build");
-app.use(express.static(buildPath));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// GET route for the root URL.
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
